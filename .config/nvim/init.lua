5e276f3a145d69fc325a94aa0a7b34980a74d324
Da-shou <fazlijaylli@gmail.com>
2025-09-10 13:52:57 +0200

    Indentation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,4 @@
 -- Basic VIM options
-vim.o.number = true
 vim.o.relativenumber = false
 vim.o.signcolumn = "yes"
 vim.o.wrap = true
@@ -8,6 +7,7 @@ vim.o.swapfile = false
 vim.g.mapleader = " "
 vim.o.winborder = "rounded"
 
+vim.cmd("set cmdheight=0")
 
 -- Keybinding for sourcing
 vim.keymap.set('n', '<leader>o', ':update | :source %<CR>')
@@ -21,20 +21,31 @@ vim.pack.add({
 	{ src = "https://github.com/williamboman/mason.nvim" },
 	{ src = "https://github.com/williamboman/mason-lspconfig.nvim" },
 	{ src = "https://github.com/xiyaowong/transparent.nvim" },
+	{ src = "https://github.com/nvim-lualine/lualine.nvim" },
 })
 
 -- Mason setup, specfiy which LSP binaries to install.
 require "mason".setup()
-require "mason-lspconfig".setup()
+require "mason-lspconfig".setup({
+	ensure_installed = {
+		'lua_ls',
+		'pyright',
+		'clangd'
+	},
+	automatic_enable = true
+})
 
 -- mini.nvim addons. Really useful (and fast) stuff !
-require "mini.pick".setup() -- File searcher
+require "mini.pick".setup()    -- File searcher
 require "mini.starter".setup() -- Pretty start screen
-require "mini.notify".setup() -- Notification window for LSP
-require "mini.icons".setup() -- File icons
-require "mini.files".setup() -- File explorer
-require "mini.pairs".setup() -- Automatic character pairs
+require "mini.notify".setup()  -- Notification window for LSP
+require "mini.icons".setup()   -- File icons
+require "mini.files".setup()   -- File explorer
+require "mini.pairs".setup()   -- Automatic character pairs
 require "mini.tabline".setup() -- Allow multiple tabs
+
+-- Prettier and more useful status bar
+require "lualine".setup()
 
 vim.keymap.set('n', '<leader>f', ":Pick files<CR>")
 vim.keymap.set('n', '<leader>h', ":Pick help<CR>")
