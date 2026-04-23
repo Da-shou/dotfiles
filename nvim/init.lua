08dd2154445c1460174349dc25b29331fa0b36f8
Da-shou <fazlijaylli@gmail.com>
2026-04-23 16:57:43 +0200

    Made custom icons

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,6 +73,54 @@ require "blink.cmp".setup({
 	fuzzy = { implementation = "prefer_rust" }
 })
 
+-- Pretty devicons for statusline and nvim-tree
+require "nvim-web-devicons".setup({
+	override_by_filename = {
+		["CMakeLists.txt"] = {
+			color = "#5590de",
+			icon = "",
+			name = "CMakeLists.txt"
+		},
+		["Makefile"] = {
+			color = "#ca8888",
+			icon = "",
+			name = "Makefile"
+		},
+		[".gitignore"] = {
+			color = "#f05133",
+			icon = "󱞃",
+			name = ".gitignore"
+		},
+	};
+	override_by_extension = {
+		["exe"] = {
+			icon = "󰖳",
+			color = "#11bbff",
+			name = "windows-executable"
+		},
+		["glsl"] = {
+			icon = "󰠱",
+			color = "#eedd88",
+			name = "gl-shader-file"
+		},
+		["png"] = {
+			icon = "",
+			color = "#00aaaa",
+			name = "png-image"
+		},
+		["c"] = {
+			icon = "",
+			color = "#5577bb",
+			name = "c-source"
+		},
+		["h"] = {
+			icon = "",
+			color = "#995599",
+			name = "c-header"
+		},
+	};
+})
+
 -- File explorer
 require "nvim-tree".setup({
 	renderer = {
@@ -106,8 +154,6 @@ require "mini.notify".setup({
 	},
 }) -- Notification window for LSP
 
--- Pretty devicons for statusline
-require "nvim-web-devicons".setup()
 
 -- Prettier statusline
 require "lualine".setup({
